86787766fce191b783bfb7943f9620624a7fecc6
ams4222 <ams4222@g.rit.edu>
2023-03-04 17:04:29 -0500

    made key classes and updated main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+#from directkeys import PressKey, ReleaseKey
+#from directkeys import space_pressed
 
 detector = HandDetector(detectionCon = 0.8, maxHands = 1)
 
@@ -9,15 +11,20 @@ video = cv2.VideoCapture(0)
 while True:
     ret, frame = video.read()
     hands, img = detector.findHands(frame)
+    cv2.rectangle(img, (0, 480), (300, 425), (0, 0, 0), -2)
+    cv2.rectangle(img, (640, 480), (300, 425), (0, 0, 0), -2)
     if hands:
         lmlist = hands[0]
         fingerUp = detector.fingersUp(lmlist)
         if fingerUp == [0, 0, 0, 0, 0]:
             cv2.putText(frame, "Finger Count: 0", (20, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
+            cv2.putText(frame, "On ground", (440, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
         if fingerUp == [1, 1, 1, 1, 1]:
             cv2.putText(frame, "Finger Count: 5", (20, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
+            cv2.putText(frame, "Jumping", (440, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
         if fingerUp == [0, 1, 1, 0, 0]:
             cv2.putText(frame, "Finger Count: 2", (20, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
+            cv2.putText(frame, "Diving", (440, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
     cv2.imshow("Frame", frame)
     k = cv2.waitKey(1)
     if k == ord("q"):
