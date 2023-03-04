af41a648cd84a094bbc158a59da1cbdaeac541c0
ams4222 <ams4222@g.rit.edu>
2023-03-04 16:00:47 -0500

    added finger count

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ video = cv2.VideoCapture(0)
 while True:
     ret, frame = video.read()
     hands, img = detector.findHands(frame)
+    if hands:
+        lmlist = hands[0]
+        fingerUp = detector.fingersUp(lmlist)
+        if fingerUp == [0, 0, 0, 0, 0]:
+            cv2.putText(frame, "Finger Count: 0", (20, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
+        if fingerUp == [1, 1, 1, 1, 1]:
+            cv2.putText(frame, "Finger Count: 5", (20, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
+        if fingerUp == [0, 1, 1, 0, 0]:
+            cv2.putText(frame, "Finger Count: 2", (20, 460),cv2.FONT_HERSHEY_COMPLEX, 1, (188, 214, 189), 1, cv2.LINE_AA)
     cv2.imshow("Frame", frame)
     k = cv2.waitKey(1)
     if k == ord("q"):
@@ -16,4 +25,5 @@ while True:
 
 
 video.release()
+
 cv2.destroyAllWindows() 
