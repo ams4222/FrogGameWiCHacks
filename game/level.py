454d5efb5678a858a20525ed5ed48b15388dcd58
ams4222 <ams4222@g.rit.edu>
2023-03-04 19:29:45 -0500

    wrote level infinite

---
+++ game/level.py
@@ -0,0 +1,35 @@
+import pygame as pg
+import math
+
+clock = pg.time.Clock()
+FPS = 60
+
+SCREEN_WIDTH = 1000 #probs should change
+SCREEN_HEIGHT = 600 #probs should change
+
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+bg = pg.image.load("game/assets/lilypad.png").convert()
+bg_width = bg.get_width()
+
+scroll = 0
+tiles = math.ceil(SCREEN_WIDTH / bg_width) + 1
+
+run = True
+while run:
+    clock.tick(FPS)
+
+    for i in range(0, tiles):
+        screen.blit(bg, (i * bg_width + scroll, 0))
+
+    scroll -= 5
+
+    if abs(scroll > bg_width):
+        scroll = 0
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+pg.display.update()
+pg.quit()
